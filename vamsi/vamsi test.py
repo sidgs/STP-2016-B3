450c815840e360b5693ba1b81d564a464ce3695c
vani92 <vani.ravanaboina@gmail.com>
2016-07-05 20:15:28 -0400

    Update vamsi test.py

--- vamsi/vamsi test.py
+++ vamsi/vamsi test.py
@@ -1,10 +1,2 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 print("hello this is vamsi")
-print ("this is what it is")
-=======
-print("hello this is vamsi")
->>>>>>> origin/Nagendra
-=======
-print("hello this is vamsi")
->>>>>>> origin/mushtysri
